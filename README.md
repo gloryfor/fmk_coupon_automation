df0a2910f535a4f4430000b2e81dfc638d21c528
gloryfor <gloryfor@gmail.com>
2023-03-12 21:41:07 +0900

    naver, google 로그인

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 알아둘것
 * chromedriver는 각 arch별로 다운받아 지정할것 (default: arm64)
 * https://chromedriver.chromium.org/downloads
+* Naver, Google 로그인 안됨
 
 사전준비
 * python3 -m venv env
 * source env/bin/activate
 * pip3 install -r requirements.txt
 * deactivate
+
+Chrome 현재창에서 selenium 스크립트 실행을 위해 
+* /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
--- main.py
+++ main.py
@@ -2,17 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-#
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+remoteDriver = webdriver.Chrome('./misc/chrome/chromedriver', chrome_options=chrome_options)
+localDriver = webdriver.Chrome('./misc/chrome/chromedriver')
 
-driver = webdriver.Chrome('./misc/chrome/chromedriver')
-nexon_id = ""
-nexon_pw = ""
-coupon_names = ["U20아시안컵토너먼트시작", "a"]
+nexon_id_dict = [
+    {'id': 'input_id', 'pw': 'input_pw'}
+]
 
-def register_coupon(coupon_name):
+google_id_dict = [
+    {'id': 'input_id'},
+    {'id': 'input_id'},
+    {'id': 'input_id'},
+    {'id': 'input_id'}
+]
+
+coupon_names = ["U20아시안컵토너먼트시작"]
+
+
+def register_coupon(coupon_name, driver):
     sleep(2)
     driver.find_element(By.ID, 'strCoupon').clear()
     driver.find_element(By.ID, 'strCoupon').send_keys(coupon_name)
@@ -22,18 +31,56 @@ def register_coupon(coupon_name):
     driver.find_element(By.ID, 'btnCommonCfm').click()
     sleep(1)
 
-if __name__ == '__main__':
-    driver.implicitly_wait(3)
-    driver.get('https://fifamobile.nexon.com/Coupon/Index')
-    driver.find_element(By.ID, 'txtNexonID').send_keys(nexon_id)
-    driver.find_element(By.ID, 'txtPWD').send_keys(nexon_pw)
+
+def login_nexon(e_nexon, driver):
+    driver.find_element(By.ID, 'txtNexonID').send_keys(e_nexon['id'])
+    driver.find_element(By.ID, 'txtPWD').send_keys(e_nexon['pw'])
     driver.find_element(By.CSS_SELECTOR, 'div.btLogin button.button01').click()
-    for coupon_name in coupon_names:
-        register_coupon(coupon_name)
+    sleep(3)
+
+
+def login_naver(driver):
+    sleep(1)
+    driver.find_element(By.CSS_SELECTOR, 'button.btNaver').click()
+    sleep(3)
+
+
+def login_google(e_google, driver):
+    driver.find_element(By.CSS_SELECTOR, 'button.btGoogle').click()
+    sleep(1)
+    driver.find_element(By.CSS_SELECTOR, "div[data-identifier='%s']" % (e_google['id'])).click()
+    sleep(3)
+
+
+def logout(driver):
     driver.find_element(By.CSS_SELECTOR, 'button.btn_logout').click()
 
-    ## 까나리1
-    # driver.find_element(By.CSS_SELECTOR, 'button.btNaver').click()
+
+if __name__ == '__main__':
+    remoteDriver.implicitly_wait(3)
+    remoteDriver.get('https://fifamobile.nexon.com/Coupon/Index')
+
+    localDriver.implicitly_wait(3)
+    localDriver.get('https://fifamobile.nexon.com/Coupon/Index')
+
+    # Nexon 계정
+    for e_nexon in nexon_id_dict:
+        login_nexon(e_nexon, localDriver)
+        for coupon_name in coupon_names:
+            register_coupon(coupon_name, localDriver)
+        logout(localDriver)
+
+    ## Naver 계정
+    login_naver(remoteDriver)
+    for coupon_name in coupon_names:
+        register_coupon(coupon_name, remoteDriver)
+    logout(remoteDriver)
+
+    ## Google 계정
+    for e_google in google_id_dict:
+        login_google(e_google, remoteDriver)
+        for coupon_name in coupon_names:
+            register_coupon(coupon_name, remoteDriver)
+        logout(remoteDriver)
 
     sleep(5)
-
